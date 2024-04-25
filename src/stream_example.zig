e7e99b79192835a16d0e749f7e495362bedf30bd
Yuya Isaka <zxwg6329@gmail.com>
2024-04-26 01:36:52 +0900

    fix

--- src/stream_example.zig
+++ src/stream_example.zig
@@ -1,6 +1,3 @@
 const std = @import("std");
-const builtin = @import("builtin");
 
-pub fn main() anyerror!void {
-    try std.debug.print("{any}", .{builtin.os.tag == .windows});
-}
+pub fn main() anyerror!void {}
